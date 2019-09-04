c6e1c4dfd3e73c625a071e19accd4b2ee04e565c
James Terwilliger <jamest@microsoft.com>
2019-09-03 22:28:07 -0700

    Add a naive aggregate provider for aggregates that cannot be optimized

---
+++ Sources/Core/Microsoft.StreamProcessing/Aggregates/NaiveAggregate.cs
@@ -0,0 +1,26 @@
+﻿// *********************************************************************
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License
+// *********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Microsoft.StreamProcessing.Aggregates
+{
+    internal static class NaiveAggregate
+    {
+        public static NaiveAggregate<TInput, TOutput> Generate<TInput, TOutput>(Expression<Func<List<TInput>, TOutput>> resultConstructor)
+            => new NaiveAggregate<TInput, TOutput>(resultConstructor);
+    }
+
+    internal sealed class NaiveAggregate<TInput, TOutput> : ListAggregateBase<TInput, TOutput>
+    {
+        private readonly Expression<Func<List<TInput>, TOutput>> createResult;
+
+        public NaiveAggregate(Expression<Func<List<TInput>, TOutput>> resultConstructor)
+            => this.createResult = resultConstructor;
+
+        public override Expression<Func<List<TInput>, TOutput>> ComputeResult() => createResult;
+    }
+}
